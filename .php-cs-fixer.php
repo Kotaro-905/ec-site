64d7c7b41838065bd25aedf370d630ed9b30d6d6
kotaro yamaga <postto9166@gmail.com>
2025-10-21 20:03:40 +0900

    php-fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,40 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+    ->name('*.php')
+    ->notName('_ide_helper.php')
+    ->exclude(['storage', 'vendor'])
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setFinder($finder)
+    ->setIndent('    ') // スペース4
+    ->setLineEnding("\n")
+    ->setRules([
+        '@PSR12' => true,
+        // よく使う追加ルール
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => ['default' => 'align_single_space_minimal'],
+        'concat_space' => ['spacing' => 'one'],
+        'explicit_string_variable' => true,
+        'fully_qualified_strict_types' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'phpdoc_align' => ['align' => 'left'],
+        'phpdoc_order' => true,
+        'phpdoc_summary' => false,
+        'return_type_declaration' => ['space_before' => 'none'],
+        'single_quote' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
+        'yoda_style' => false,
+    ]);
